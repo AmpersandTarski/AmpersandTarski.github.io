5353c1bb958cbfe92986dcd348d02c9ff5482615
Michiel Stornebrink <michielstornebrink@gmail.com>
2022-11-18 16:59:33 +0100

    Add placeholder folder customized sidebar from Ampersand compiler repo

---
+++ ampersand-docs/docs/Ampersand/sidebar.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sidebar: [
+      // Normal syntax:
+      {
+        type: 'doc',
+        id: 'Ampersand/Test', // document ID
+        label: 'Test', // sidebar label
+      },
+    ],
+  };
