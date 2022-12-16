36d50df6debcefa376a7906382eae9553fd65fe0
hanjoosten <han.joosten.han@gmail.com>
2022-12-16 14:35:05 +0100

    Don't break on broken links. (temporarily, until guards are in place)

--- ampersand-docs/docusaurus.config.js
+++ ampersand-docs/docusaurus.config.js
@@ -14,14 +14,14 @@ const config = {
   tagline: 'check out the latest compiler on https://github.com/AmpersandTarski/Ampersand/releases/latest',
   url: 'https://ampersandtarski.github.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/ampersand-logo.jpg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AmpersandTarski', // Usually your GitHub org/user name.
-  projectName: 'DocuGen', // Usually your repo name.
+  projectName: 'AmpersandTarski.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -101,7 +101,7 @@ const config = {
               },
             ],
           },
-          
+
           {
             title: 'More',
             items: [
